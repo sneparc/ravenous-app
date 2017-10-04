e47365fe05e48aff9c4f68a245791b431c930fa5
Casey Snepar <sneparc.gmail.com>
2017-10-04 13:49:06 -0400

    first commit

---
+++ ravenous/src/components/Business/Business.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Business.css';
+
+
+
+class Business extends React.Component{
+	render(){
+	return (
+				<div className="Business">
+		  <div className="image-container">
+		    <img src={this.props.businesses.imageSrc} alt=''/>
+		  </div>
+		  <h2>{this.props.businesses.name}</h2>
+		  <div className="Business-information">
+		    <div className="Business-address">
+		      <p>{this.props.businesses.address}</p>
+		      <p>{this.props.businesses.city}</p>
+		      <p>{this.props.businesses.state} {this.props.businesses.zipCode}</p>
+		    </div>
+		    <div className="Business-reviews">
+		      <h3>{this.props.businesses.category}</h3>
+		      <h3 className="rating">{this.props.business.rating}</h3>
+		      <p>{this.props.businesses.reviewCount} reviews</p>
+		    </div>
+		  </div>
+		</div>
+		);
+	}
+
+};
+
+export default Business;
